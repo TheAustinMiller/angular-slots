e595a7d0b2adad86e1eeb8a03631d7d77d5a5683
TheAustinMiller <themilleraustin@gmail.com>
2025-07-20 19:16:10 -0500

    Base code implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SlotMachineComponent } from './slot-machine/slot-machine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlotMachineComponent
   ],
   imports: [
     BrowserModule
